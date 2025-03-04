357a82c4beb8c80a76929e4069adada6e00532fa
nocturnalelk07 <finnp@outlook.com>
2025-03-04 22:34:22 +0000

    turret has a base and barrel, input system started

---
+++ Assets/Scripts/turretAimingBehaviour.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretAimingBehaviour : MonoBehaviour
+{
+    //this is the aiming class for the turret that the player controls
+
+
+}
